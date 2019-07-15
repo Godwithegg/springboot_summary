cdffe113ec6eae70bb3c1374bb4f3cf91d081895
Godwithegg <675175647@qq.com>
2019-07-15 17:49:03 +0800

    使用模板引擎的crud项目

---
+++ spring-boot-04-web-restfulcrud/src/main/java/com/danhuang/springboot/exception/UserNotExistException.java
@@ -0,0 +1,9 @@
+package com.danhuang.springboot.exception;
+
+public class UserNotExistException extends RuntimeException{
+
+    public UserNotExistException(){
+        super("用户不存在");
+    }
+
+}
